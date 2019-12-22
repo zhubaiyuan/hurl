7de66fbd09eddc151076c573ba6bdcaa04febc90
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-22 20:19:32 +0800

    imports in mod client

---
+++ src/client.rs
@@ -0,0 +1,10 @@
+use crate::app::{App, Method, Parameter};
+use crate::errors::{Error, HurlResult};
+use log::{info, debug, trace, log_enabled, self};
+use reqwest::multipart::Form;
+use reqwest::{Client, RequestBuilder, Response, Url};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::time::Instant;
