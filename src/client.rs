cc64f73aae1ab4027f0591cd1d31377fa88b1a8e
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-22 20:36:36 +0800

    function perform_method in mod client

--- src/client.rs
+++ src/client.rs
@@ -8,3 +8,18 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use std::time::Instant;
+
+pub fn perform_method(
+    app: &App,
+    method: &Method,
+    session: &mut Option<Session>,
+) -> HurlResult<Response> {
+    let method_data = method.data();
+    perform(
+        app,
+        method.into(),
+        session,
+        &method_data.url,
+        &method_data.parameters,
+    )
+}
