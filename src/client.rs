919d705f27277287db3fb3a0ea48bd455385e38a
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-22 20:52:00 +0800

    general perform function

--- src/client.rs
+++ src/client.rs
@@ -23,3 +23,45 @@ pub fn perform_method(
         &method_data.parameters,
     )
 }
+
+pub fn perform(
+    app: &App,
+    method: reqwest::Method,
+    session: &mut Option<Session>,
+    raw_url: &str,
+    parameters: &Vec<Parameter>,
+) -> HurlResult<Response> {
+    let client = Client::new();
+    let url = parse(app, raw_url)?;
+    debug!("Parsed url: {}", url);
+
+    let is_multipart = parameters.iter().any(|p| p.is_form_file());
+    if is_multipart {
+        trace!("Making multipart request because form file was given");
+        if !app.form {
+            return Err(Error::NotFormButHasFormFile);
+        }
+    }
+
+    let mut builder = client.request(method, url);
+    builder = handle_session(
+        builder,
+        session,
+        parameters,
+        !app.read_only,
+        &app.auth,
+        &app.token,
+    );
+    builder = handle_parameters(builder, app.form, is_multipart, parameters)?;
+    builder = handle_auth(builder, &app.auth, &app.token)?;
+
+    if log_enabled!(log::Level::Info) {
+        let start = Instant::now();
+        let result = builder.send().map_err(From::from);
+        let elapsed = start.elapsed();
+        info!("Elapsed time: {:?}", elapsed);
+        result
+    } else {
+        builder.send().map_err(From::from)
+    }
+}
