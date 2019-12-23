0f94eda4c51b4ff5b8cfcca77a07ff005728f927
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-23 22:48:40 +0800

    define the struct Session to contain the session data

--- src/session.rs
+++ src/session.rs
@@ -8,3 +8,14 @@ use std::collections::HashMap;
 use std::fs::{create_dir_all, File, OpenOptions};
 use std::io::{BufReader, BufWriter};
 use std::path::PathBuf;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Session {
+    path: PathBuf,
+    name: String,
+    host: String,
+    auth: Option<String>,
+    token: Option<String>,
+    headers: HashMap<String, String>,
+    cookies: Vec<(String, String)>,
+}
