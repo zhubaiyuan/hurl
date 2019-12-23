d96bef282b813b708c14a64a1cc568d0af34c1d4
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-23 22:04:58 +0800

    declare struct Config to hold the data that we support in teh configuration file

--- src/config.rs
+++ src/config.rs
@@ -4,3 +4,12 @@ use std::path::PathBuf;
 
 use crate::app::App;
 use crate::directories::DIRECTORIES;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub verbose: Option<u8>,
+    pub form: Option<bool>,
+    pub auth: Option<String>,
+    pub token: Option<String>,
+    pub secure: Option<bool>,
+}
