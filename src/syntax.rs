8cce9a63c6aa9585f1ef9cf27157deecb627bd75
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-24 07:56:28 +0800

    imports in syntax module

---
+++ src/syntax.rs
@@ -0,0 +1,4 @@
+use crate::errors::{Error, HurlResult};
+use syntect::highlighting::ThemeSet;
+use syntect::parsing::syntax_definition::SyntaxDefinition;
+use syntect::parsing::{SyntaxSet, SyntaxSetBuilder};
