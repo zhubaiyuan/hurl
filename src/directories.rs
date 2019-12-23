43e974aea55db677666306839c8b20903f85f887
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-12-23 22:18:04 +0800

    impl method new and config to Directories type

--- src/directories.rs
+++ src/directories.rs
@@ -7,3 +7,24 @@ use std::env;
 pub struct Directories {
     config: PathBuf,
 }
+
+impl Directories {
+    fn new() -> Option<Directories> {
+        #[cfg(target_os = "macos")]
+        let config_op = env::var_os("XDG_CONFIG_HOME")
+            .map(PathBuf::from)
+            .filter(|p| p.is_absolute())
+            .or_else(|| dirs::home_dir().map(|d| d.join(".config")));
+
+        #[cfg(not(target_os = "macos"))]
+        let config_op = dirs::config_dir();
+
+        let config = config_op.map(|d| d.join("hurl"))?;
+
+        Some(Directories { config })
+    }
+
+    pub fn config(&self) -> &Path {
+        &self.config
+    }
+}
